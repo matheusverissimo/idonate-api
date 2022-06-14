a0a7c2709cf4beeacba2f61c3cb0ff9793f5e02a
Matheus Veríssimo <matheusrover13@gmail.com>
2022-06-14 20:41:53 -0300

    setup

---
+++ src/main/java/br/com/uff/dac/idonateapi/IdonateApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.uff.dac.idonateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdonateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdonateApiApplication.class, args);
+	}
+
+}
