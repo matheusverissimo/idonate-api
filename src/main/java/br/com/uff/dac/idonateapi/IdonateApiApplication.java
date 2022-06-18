6f30e9b1fd06e84d42865385dfe270d944a2d8f0
jorge carvalho <jorge.carvalho@marlo.com.au>
2022-06-17 22:46:07 -0300

    add swagger dependency to idonate api

--- src/main/java/br/com/uff/dac/idonateapi/IdonateApiApplication.java
+++ src/main/java/br/com/uff/dac/idonateapi/IdonateApiApplication.java
@@ -3,9 +3,11 @@ package br.com.uff.dac.idonateapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableWebMvc
 public class IdonateApiApplication {
 
 	public static void main(String[] args) {
